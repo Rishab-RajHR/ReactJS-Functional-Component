fc8fe3e57550d808d9477d046e913ad258f45b5d
Rishab Raj <rr0948555@gmail.com>
2025-11-23 14:10:04 +0530

    Functional Component

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FuncComponent from './FuncComponent'
+
+const App = () => {
+  return (
+    <div>
+       <h1>This is my App.jsx File</h1>
+       <FuncComponent />
+       <FuncComponent />
+       <FuncComponent />
+
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/FuncComponent.jsx
@@ -0,0 +1,14 @@
+function FuncComponent(){
+// export function FuncComponent(){
+   return(
+    <div>
+
+        <h1>This is my First Function Based Component</h1>
+        <h2>Second Heading</h2>
+        <h3>Third Heading</h3>
+    </div>
+    
+   )
+}
+
+export default FuncComponent;
